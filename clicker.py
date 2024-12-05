ed39198e617b067c4b493645ea67aa039d0b3b17
sinanefeozler <sinanefeozler88@gmail.com>
2024-12-05 16:00:31 +0300

    Update clicker.py

--- clicker.py
+++ clicker.py
@@ -2,39 +2,58 @@ from pynput.mouse import Listener,Controller,Button
 import os
 import threading
 import time
+
 def clearConsole():
-    command = 'clear'
     if os.name in ('nt', 'dos'):
-        command = 'cls'
-    os.system(command)
+        os.system('cls')
+    else:
+        os.system('clear')
+
 class Clicker(threading.Thread):
     def __init__(self,button):
         super().__init__()
         self.running = False
-        self.p_running = True
         self.button = button
+        self.macro = False
+    
     def run(self):
-        while self.p_running:
+        while True:
             time.sleep(0.1)
-            while self.running:
+            while self.running and self.macro:
                 mouse.click(self.button)
-                time.sleep(0.05) # change to adjust the cps(clik per second)
+                time.sleep(0.05)
+
 mouse = Controller()
 click_right = Clicker(Button.right)
 click_left = Clicker(Button.left)
 click_left.start()
 click_right.start()
 
+print(f"Right macro : {click_right.macro}\nLeft macro : {click_left.macro}")
 def on_click(x,y,button,pressed):
     if pressed:
-        if button.name == "x1":
+        if button.name == "right":
             click_right.running = True
-        if button.name == "x2":
+        if button.name == "left":
             click_left.running = True
+        if button.name == "button9": # change this to adjust the starter
+            if click_left.macro:
+                click_left.macro = False
+            else:
+                click_left.macro = True
+            clearConsole()
+            print(f"Right macro : {click_right.macro}\nLeft macro : {click_left.macro}")
+        if button.name == "button8": # change this to adjust the starter
+            if click_right.macro:
+                click_right.macro = False
+            else:
+                click_right.macro = True
+            clearConsole()
+            print(f"Right macro : {click_right.macro}\nLeft macro : {click_left.macro}")
     else:
-        if button.name == 'x1':
+        if button.name == 'right':
             click_right.running = False
-        if button.name == 'x2':
+        if button.name == 'left':
             click_left.running = False
 
 with Listener(on_click=on_click) as listener:
